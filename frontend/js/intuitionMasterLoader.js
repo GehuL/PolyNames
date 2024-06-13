9a3aba6b162df6b9c9cf7745391d1b262a63b767
Gehu <lauric.gehu@free.fr>
2024-06-13 17:12:59 +0200

    modif css couleur carte

--- frontend/js/intuitionMasterLoader.js
+++ frontend/js/intuitionMasterLoader.js
@@ -3,14 +3,6 @@ import { CardsView } from "./views/cards-view.js";
 function onLoad()
 {
     const view = new CardsView();
-
-    const select_nbr = document.getElementById("nombre_indice");
-    for(let i = 1; i < 10; i++)
-    {
-        const option = document.createElement("option");
-        option.text = i;
-        select_nbr.add(option);
-    }
 }
 
 window.addEventListener("load", onLoad);
--- frontend/js/views/cards-view.js
+++ frontend/js/views/cards-view.js
@@ -5,13 +5,15 @@ export class CardsView
         this.#displayCards()
     }
 
-    #displayCards(product)
+    #displayCards(cardslist)
     {
         const cards = document.getElementsByClassName("cards")[0];
 
         for(let i = 0; i < 25; i++)
         {
             const card = document.createElement("div");
+            card.setAttribute("id", 1);
+            card.dataset.color = "GRIS";
             card.classList.add("card");
             card.innerHTML = "TEST";
             cards.appendChild(card);
