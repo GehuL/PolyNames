b338991f33de216226c723773b74e9dc8e363454
LouisDecouzon <louis.decouzon.pro@gmail.com>
2024-06-14 17:19:15 +0200

    alert si code==500 dans guess

--- frontend/js/intuitionMasterLoader.js
+++ frontend/js/intuitionMasterLoader.js
@@ -25,7 +25,10 @@ async function guess(word){
     const id_partie=JSON.parse(localStorage.getItem("current_player"))
     const guess = await fetch("http://localhost:8080/guess/"+id_partie.idPartie,{method:"post",headers: {"Content-Type": "application/json"},body:JSON.stringify(word)})
     if(guess.status==200){
-
+        console.log("indice envoyé")
+    }
+    else{
+        alert(guess.text())
     }
 }
 
